5e146da179ea719162295893a447ce4304932244
hak86 <hakamelassouad@gmail.com>
2020-07-23 15:52:59 +0200

    prague: inherit treble_common

--- lineage_prague.mk
+++ lineage_prague.mk
@@ -26,6 +26,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
 
 # Inherit from prague device
 $(call inherit-product, device/huawei/prague/device.mk)
