99a6bd33efba11da475304f6fae64c4dd173593f
hak86 <hakamelassouad@gmail.com>
2020-07-24 22:26:20 +0200

    prague: nuke treble_common configs

--- lineage_prague.mk
+++ lineage_prague.mk
@@ -27,7 +27,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
 
 TARGET_AOSP_BASED := true
 
