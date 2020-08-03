77c92a51c4c2494e6c0bc950c1f58d133393f9a4
hak86 <hakamelassouad@gmail.com>
2020-08-03 13:14:03 +0200

    prague: inherit aosp_arm64_ab

--- lineage_prague.mk
+++ lineage_prague.mk
@@ -24,6 +24,7 @@
 # components.
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64_ab.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
